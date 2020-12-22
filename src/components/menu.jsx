92eb0e066ad48175a5071eab28f7718eb67703a6
akylzynpcnr <zeynepakyel92@gmail.com>
2020-12-22 20:32:26 +0300

    1 file modified

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,8 +3,38 @@ import React from "react";
 export default () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <span className="navbar-brand">Harry Potter</span>
+      <a className="navbar-brand" href="#">
+        Harry Potter
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Anasayfa <span className="sr-only" />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Ürünler
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Fiyatlar
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
